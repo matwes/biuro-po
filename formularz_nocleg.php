dec849c11a9e89be68adaf9c5ebf149b62879ef3
Mateusz <mateusz.jan.wesolowski@gmail.com>
2017-01-25 23:59:03 +0100

    Dorobione kalendarze, brakuje tylko POSTa

--- formularz_nocleg.php
+++ formularz_nocleg.php
@@ -10,11 +10,17 @@ if (session_status() == PHP_SESSION_NONE) {
 <!DOCTYPE html>
 <html lang ="pl">
 	<head>
-		<link rel="stylesheet" type="text/css" href="style.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		
 		<meta charset="utf-8">
 		<title>Dodawanie nowego zapytania o usługę noclegową</title>
+		
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker.min.css" rel="stylesheet"/>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.min.js"></script>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>	
 		<script type="text/javascript">
             $(function() {
@@ -31,9 +37,6 @@ if (session_status() == PHP_SESSION_NONE) {
                 }); 
             });
         </script>
-		
-		 
-
 	</head>
 
 	<body onload="codeAddress();">
@@ -41,7 +44,7 @@ if (session_status() == PHP_SESSION_NONE) {
 			<div class="jumbotron" style="padding: 70px;">
 				<h2 align= "center">Dodawanie nowego zapytania o usługę noclegową</h2>
 				<br><br>
-				<form class="form-horizontal" role="form" method="post" action="Form.php">
+				<form class="form-horizontal" role="form" method="post" action="formularz_nocleg.php" style="width:70%; margin: 0 auto;">
 					<div class="form-group">
 						<label for="kraj" class="col-sm-2 control-label">Kraj</label>
 						<div class="col-sm-10">
@@ -52,9 +55,10 @@ if (session_status() == PHP_SESSION_NONE) {
 						</div>
 						<div id="miasta">
 						</div>
+						
 						<label for="stars" class="col-sm-2 control-label" style="padding-top: 15px;">Standard</label><br>
 						<div class="col-sm-10">
-							<fieldset class="rating" id="stars">
+							<fieldset class="rating" id="stars" name="stars">
 								<input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
 								<input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
 								<input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
@@ -62,23 +66,23 @@ if (session_status() == PHP_SESSION_NONE) {
 								<input type="radio" id="star1" name="rating" value="1" checked="checked"/><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
 							</fieldset>
 						</div>
-						<label for="seats" class="col-sm-2 control-label">Liczba miejsc:</label>
+						
+						<label for="seats" class="col-sm-2 control-label">Liczba miejsc</label>
 						<div class="col-sm-10">
-							<input type="text" class="form-control bfh-number" id="seats">
+							<input type="text" class="form-control bfh-number" id="seats" name="seats"><br><br>
 						</div>
 						
+						<label for="start" class="col-sm-2 control-label">Data wyjazdu</label>
+						<div class="col-sm-4">
+							<input class="form-control" data-provide="datepicker" id="start" name="start">
+						</div>
 						
-						
-
-						
-						
-						
-						
-						
-						
+						<label for="end" class="col-sm-2 control-label">Data powrotu</label>
+						<div class="col-sm-4">
+							<input class="form-control" data-provide="datepicker" id="end" name="end">
+						</div>	
 					</div>
 					<br><br>
-					
 					
 					<div style="width:90%; margin: 0 auto;">
 						<div class='test'>
@@ -94,5 +98,25 @@ if (session_status() == PHP_SESSION_NONE) {
 				</form>
 			</div>
 		</div>
+		<?php
+			if(isset($_POST["submit"]))
+			{
+				if (session_status() == PHP_SESSION_NONE){
+					session_start();
+				}
+				$_SESSION["kraj"] = $funkcje->zczytajWartosc($_POST, $_SESSION, "kraj"); 
+				$_SESSION["ctySelect"] = $funkcje->zczytajWartosc($_POST, $_SESSION, "ctySelect"); 
+				$_SESSION["stars"] = $funkcje->zczytajWartosc($_POST, $_SESSION, "stars"); 
+				$_SESSION["seats"] = $funkcje->zczytajWartosc($_POST, $_SESSION, "seats"); 
+				$_SESSION["start"] = $funkcje->zczytajWartosc($_POST, $_SESSION, "start"); 
+				$_SESSION["end"] = $funkcje->zczytajWartosc($_POST, $_SESSION, "end"); 
+				//header("Location: zapytanie.php");
+				exit;
+			/*else{
+				echo "<script type='text/javascript'>alert('$message');</script>";
+			}*/
+	   }
+
+	?>
 	</body>
 </html>
