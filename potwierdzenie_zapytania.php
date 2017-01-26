efe063f4ce05753da229d71214d4973dd3e483c0
Mateusz <mateusz.jan.wesolowski@gmail.com>
2017-01-26 21:34:04 +0100

    Różne zmiany i dodwanie usługi noclegowej już działa

---
+++ potwierdzenie_zapytania.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once 'funkcje.php';
+
+$funkcje = new Funkcje();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+?>
+<!DOCTYPE html>
+<html lang ="pl">
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<meta charset="utf-8">
+		<title>Dane nowego wpółpracownika</title>
+	</head>
+	<body>
+
+		<div class="container">
+			<div class="jumbotron">
+				<h2 align="center">Dane nowego współpracownika</h2>
+				<br/><br/>
+				
+				<div class="row" style="width: 50%; margin:0 auto;">
+					<div class = 'form-horizontal well'>
+						<fieldset>
+							<div class="form-horizontal">
+								<div>
+									<label for="kraj" class="col-sm-4 control-label">Wybrany kraj</label>
+									<div class="col-sm-8">
+										<p class="nowyWsp" id="kraj" name="kraj"><?php echo $funkcje->pobierzKraj($_SESSION["kraj"])?></p>
+									</div>
+									
+									<label for="miasto" class="col-sm-4 control-label">Miasto</label>
+									<div class="col-sm-8">
+										<p class="nowyWsp" id="miasto" name="miasto"><?php echo $funkcje->pobierzMiasto($_SESSION["ctySelect"])?></p>
+									</div>
+									<label for="rating" class="col-sm-4 control-label">Standard</label>
+									<div class="col-sm-8">
+										<p class="nowyWsp" id="rating" name="rating"><?php echo $_SESSION["rating"] ?>-gwiazdkowy</p>
+									</div>
+									<label for="seats" class="col-sm-4 control-label">Liczba miejsc</label>
+									<div class="col-sm-8">
+										<p class="nowyWsp" id="seats" name="seats""><?php echo $_SESSION["seats"] ?></p>
+									</div>
+									<label for="start" class="col-sm-4 control-label">Data wyjazdu</label>
+									<div class="col-sm-8">
+										<p class="nowyWsp" id="start" name="start"><?php echo $_SESSION["start"] ?></p>
+									</div>
+									<label for="end" class="col-sm-4 control-label">Data powrotu</label>
+									<div class="col-sm-8">
+										<p class="nowyWsp" id="end" name="end"><?php echo $_SESSION["end"] ?></p>
+									</div>
+								</div>
+							</div>
+						</fieldset>
+					</div>
+				</div>
+									
+				<div style="width:90%; margin: 0 auto;">
+					<h5 align="center">Zatwierdź poprawność danych</h5><br>
+						<div class='test'>
+							<div style='float: left;'>
+								<button onclick="location.href = 'formularz_nocleg.php';" type="button" class="btn btn-primary btn-lg pull-right">Wróc i popraw</button>
+							</div>
+							<div style='float: right;'>
+								<button onClick="location.href = 'dodaj_zapytanie.php'" type="button" class="btn btn-primary btn-lg pull-right">Zatwierdź</button>
+							</div>
+						</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
